60168771951b87b3352de86b073f5db01caa9908
sammuraii <samarth2001@gmail.com>
2022-05-06 16:04:44 +0400

    Co-authored-by: RoshniMathur31 <RoshniMathur31@users.noreply.github.com> Co-authored-by: Ahmed Abdul Saad <xosaad@users.noreply.github.com>

--- Craftable.java
+++ Craftable.java
@@ -1,26 +1,32 @@
 public abstract class Craftable{
-    final void craftItem(){
-    UseWood(); //common
-    UseLeaves();//common
-    if(UsingStones()){
-        RubbingStones();
+    final void craftItem()
+    {
+        UseWood(); //common
+        UseLeaves();//common
+        if(UsingStones()){
+            RubbingStones();
+        }
+        Completed();
+        
     }
     
 // for creating fire -> place wood -> placeLeaf-> rub stones (sensor) (blow on the fire to make it going, use mic)
 // for creating house -> place wood -> place roof
 
-    protected void UseWood() 
-    { 
-        UI.print("")
-    }s
 
-    protected void UseLeave() 
+	protected void UseWood() 
     { 
+        UI.print("placing wood");
     }
 
-    protected void RubbingStones() 
+    protected void UseLeaves() 
     { 
+        UI.print("using leaves");
     }
-    abstract Rubbing
+    private boolean UsingStones(){
+        return false;
+    }
 
+    abstract void Completed();
+    abstract void RubbingStones();
 }
